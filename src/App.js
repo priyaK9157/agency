002e245f05cbc0fe36ffab5c5cef5f092e7bcc35
priyaK9157 <priyakumari2003004@gmail.com>
2023-12-25 18:56:17 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
+import Home from './component/Home';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     
   );
