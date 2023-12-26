3ddfbad6660712a2582b2c69435867953ae782be
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2023-12-26 08:29:24 +0530

    influencerdeatail.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
 import Influencer from './component/Influencer/Influencer';
+import InfluencerDetail from './component/Influencer/InfluencerDetail';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path='/influencerPlan' element={<Influencer/>}/>
+        <Route path='/influencerDetail' element={<InfluencerDetail/>}/>
       </Routes>
     
   );
