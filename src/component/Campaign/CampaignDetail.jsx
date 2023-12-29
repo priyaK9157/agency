e7efeaad7bf30f3262661470040101d503744bce
priyaK9157 <priyakumari2003004@gmail.com>
2023-12-29 10:15:28 +0530

    full commit

--- src/component/Campaign/CampaignDetail.jsx
+++ src/component/Campaign/CampaignDetail.jsx
@@ -36,33 +36,6 @@ const CampaignDetail = () => {
 ]
   return (
     <div>
-      {/* <div className='flex justify-around mt-3'>
-            <p className='text-3xl font-bold'>CampaignDetail</p>
-            <div className='flex items-center border'>
-            <FaSearch /><input placeholder='Search' className=' px-5'></input>
-            </div>
-            <div className='flex gap-2'>
-                <button className='text-[#6B8DE6]'>MASSAGES</button>
-                <button className='text-[#6B8DE6] border border-[#6B8DE6] rounded-lg p-2'>ANALYSIS</button>
-                <button className='bg-[#6B8DE6] text-white p-2 rounded-lg'>INVITE MORE</button>
-            </div>
-        </div>
-
-        <div className='flex justify-between m-4 px-1'>
-            <p className='flex items-center text-[#6B8DE6]'><FaArrowLeft />Back</p>
-            <div className='flex gap-3'>
-                <button className='bg-[#0288D1] p-2 rounded-lg text-white'>PAYMENT RECORD</button>
-                <button className='bg-[#6B8DE6] p-2 rounded-lg text-white'>SHARE WITH BRAND</button>
-            </div>
-        </div>
-
-        <div className='flex gap-3 ml-5'>
-            <img src={img} alt="Campaign Image" />
-            <div className='flex flex-col gap-2'>
-              <h1 className='text-2xl font-bold'>{names}</h1>
-              <p>{para}</p>
-            </div>
-        </div> */}
         <Navbar/>
         <div className=' mt-10'>
              <div className=' flex justify-around'>
