69e34a48b11388c0a048e5a0b0feb68e59b8215b
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2023-12-31 11:23:33 +0530

    hoem commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,39 +7,71 @@ import { FaArrowTrendUp } from "react-icons/fa6";
 import Navbar from './common/Navbar';
 import { FaArrowTrendDown } from "react-icons/fa6";
 import block1 from '../asset/Block1.jpg'
-
+import { Doughnut } from 'react-chartjs-2';
+import image1 from "../Assets/Screenshot 2023-12-31 102305.png"
+import { PieChart } from '@mui/x-charts/PieChart';
 const Home = () => {
+    const testData = [
+        { label: 'United States  38.6%', value: 38.6, color: '#6B8DE6' },
+        { label: 'Canada   22.5%', value: 22.5, color: '#00CF7F' },
+        { label: 'Mexico  30.8%', value: 30.8, color: '#FF7361' },
+        { label: 'Other   8.1%', value: 8.1, color: '#B659FF' },
+      ];
   return (
     <div>
         <Navbar/>
         <div className='flex flex-col gap-6 justify-between mt-7'>
            
-            <div className='flex justify-center px-14'>
-                <img src={Frame19} className='w-[60rem] h-[15rem]'></img>
-                <img src={Frame20} className='w-[60rem] h-[15rem]'></img>
+            <div className='flex justify-center '>
+                <img src={Frame19} className='w-[730px] h-[265px]'></img>
+                <img src={Frame20} className='w-[481px] h-[265px]'></img>
             </div>
 
-            <div className='flex justify-center gap-20'>
+            <div className='flex justify-center gap-14 items-center -ml-7'>
                 <div className='flex gap-2'>
-                    <div className='bg-[#E3F5FF] py-4 px-7 rounded-xl shadow-lg h-24'>
-                        <p className='font-bold '>views</p>
-                        <p className='flex'><span className='font-bold'>721K</span><p>+11.01%</p><FaArrowTrendUp /></p>
+                    <div className='bg-[#E3F5FF] py-4 px-7 rounded-xl shadow-lg h-32 p-2 flex flex-col  '>
+                        <p className='font-bold  mt-2 '>views</p>
+                        <div className=' flex justify-between  gap-4 mt-3 '>
+                              <h1 className=' text-xl font-semibold '>721k</h1>
+                              <div className=' flex gap-1 items-center'>
+                              <p>+11.01%</p> 
+                              <FaArrowTrendUp/>
+                              </div>
+                        </div>
                     </div>
-                    <div className='bg-[#E5ECF6] p-5 rounded-xl shadow-lg h-24'>
-                        <p className='font-bold '>Visits</p>
-                        <p className='flex'><span className='font-bold'>367K</span><p>-0.03%</p><FaArrowTrendUp /></p>
+                    <div className='bg-[#E5ECF6] py-4 px-7 rounded-xl shadow-lg h-32 p-2 flex flex-col  '>
+                        <p className='font-bold mt-2 '>Visits</p>
+                        <div className=' flex justify-between  gap-4 mt-3 '>
+                              <h1 className=' text-xl font-semibold '>367K</h1>
+                              <div className=' flex gap-1 items-center'>
+                              <p>-0.03%</p> 
+                              <FaArrowTrendUp/>
+                              </div>
+                        </div>
                     </div>
-                    <div className='bg-[#E3F5FF] p-5 rounded-xl shadow-lg h-24'>
-                        <p className='font-bold '>New Users</p>
-                        <p className='flex'><span className='font-bold'>1,156</span><p>+15.03%</p><FaArrowTrendDown /></p>
+                    <div className='bg-[#E5ECF6] py-4 px-7 rounded-xl shadow-lg h-32 p-2 flex flex-col  '>
+                        <p className='font-bold mt-2 '>New Users</p>
+                        <div className=' flex justify-between  gap-4 mt-3 '>
+                              <h1 className=' text-xl font-semibold '>1,156</h1>
+                              <div className=' flex gap-1 items-center'>
+                              <p>+15.03%</p> 
+                              <FaArrowTrendUp/>
+                              </div>
+                        </div>
                     </div>
-                    <div className='bg-[#E5ECF6] p-5 rounded-xl shadow-lg h-24'>
-                        <p className='font-bold'>Active Users</p>
-                        <p className='flex'><span className='font-bold'>239K</span><p>+6.08%</p><FaArrowTrendUp /></p>
+                    <div className='bg-[#E5ECF6] py-4 px-7 rounded-xl shadow-lg h-32 p-2 flex flex-col  '>
+                        <p className='font-bold mt-2 '>Active Users</p>
+                        <div className=' flex justify-between  gap-4 mt-3 '>
+                              <h1 className=' text-xl font-semibold '>239K</h1>
+                              <div className=' flex gap-1 items-center'>
+                              <p>+6.08%</p> 
+                              <FaArrowTrendUp/>
+                              </div>
+                        </div>
                     </div>
                 </div>
 
-                <div>
+                <div className='  p-4 shadow-xl '>
                     <p className='font-bold'>Active Inflencers</p>
                     <div>
                         <div className='flex p-2 gap-2'>
@@ -59,31 +91,28 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            {/* <CChart
-                type="doughnut"
-                data={{
-                    labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-                    datasets: [
-                    {
-                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-                        data: [40, 20, 80, 10],
-                    },
-                    ],
-                }}
-                options={{
-                    plugins: {
-                    legend: {
-                        labels: {
-                        color: getStyle('--cui-body-color'),
-                        }
-                    }
-                    },
-                }}
-                /> */}
-            {/* <div>
-                <img src={block1}></img>
-            </div> */}
-            <div></div>
+                 
+            <div className=' flex justify-around items-center mx-auto w-10/12   gap-10 '>
+                       <div className=' translate-x-20 translate-y-6'>
+                             <img src={image1} width="760px" height="246px"/>
+                       </div>
+                     
+                       <PieChart
+                                series={[
+                                {
+                                    paddingAngle: 5,
+                                    innerRadius: 47,
+                                    outerRadius: 80,
+                                    data:testData,
+                                },
+                                ]}
+                                margin={{ right: 300 }}
+                                width={200}
+                                height={200}
+                               
+                            />
+                      
+            </div>
         </div>
     </div>
   )
